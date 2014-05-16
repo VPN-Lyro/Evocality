f570c0ae83f8b6a28571c3dd387fcb7a8bebe73a
Lyro Bay <z_legendz_x@hotmail.com>
2014-05-16 03:04:58 -0700

    Adding files to new repo

---
+++ app/views/template/template.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Evocality - Virtual Pet Site</title>
+
+	<link href="assets/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+	<header>
+		<div class="row">
+			<div class="medium-3 columns">
+				<a href="/" title="Evocality">
+					<img src="assets/images/logo.png" alt="Evocality" />
+				</a>
+			</div>
+		</div>
+	</header>
+	<nav>
+		<div class="row">
+			<div class="large-12 columns">
+				<ul>
+					<li class="active">
+						<a href="#">home</a>
+					</li>
+					<li>
+						<a href="#">about</a>
+					</li>
+					<li>
+						<a href="#">pets</a>
+
+						<!-- Sub Navigation -->
+						<ul class="submenu">
+							<li><a href="#">Adopt</a></li>
+							<li><a href="#">Buy</a></li>
+							<li><a href="#">Sell</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">explore</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<section class="banner">
+		<div class="slides">
+			<ul>
+				<li><img src="assets/images/header/banner/1.jpg" alt="Banner 1" /></li>
+			</ul>
+		</div>
+		<!--
+			<div class="controls">
+				<ul>
+					<li data-number="1"></li>
+				</ul>
+			</div>
+		-->
+	</section>
+	<section class="info">
+		<div class="row">
+			<div class="medium-5 columns" id="recent-updates">
+				<div class="box one">
+					<div class="box-header">
+						Recent Updates
+					</div>
+					<div class="box-content">
+						<h2>Scenery is great</h2>
+						Lorem ipsum dolor sit amet, consectetur adipiscing 
+						elit. Vivamus at molestie eros, ut sodales neque. 
+						Vestibulum sagittis massa sem, at luctus nunc 
+						varius a. Ut non est ut nisl vehicula porta.
+					</div>
+				</div>
+			</div>
+			<div class="medium-3 columns" id="recent-updates">
+				<div class="box one">
+					<div class="box-header">
+						Recent Pet
+					</div>
+					<div class="box-content">
+					</div>
+				</div>
+			</div>
+			<div class="medium-4 columns" id="recent-updates">
+				<div class="box one">
+					<div class="box-header">
+						Recent Updates
+					</div>
+					<div class="box-content">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="twitter">
+		<div class="row">
+			<div class="large-12 columns">
+				<img src="assets/images/header/twitter/icon.png" alt="Twitter" />
+			</div>
+		</div>
+	</section>
+
+	<section class="main">
+		<div class="row">
+			<div class="medium-8 columns">
+				<section class="content">
+					<h1>Evocality Pets</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum eget augue vitae adipiscing. Aenean congue tortor non ipsum congue pharetra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis id elit metus. Donec aliquam facilisis tristique. Nulla facilisi. Fusce vulputate ante sed nunc cursus, vel iaculis turpis gravida.
+					</p>
+					<p>
+						Etiam id volutpat enim. Nunc tortor nisl, consectetur rhoncus odio sit amet, tincidunt imperdiet velit. Praesent blandit dapibus augue. Donec sit amet mi eget velit mollis scelerisque. Sed sed quam eu eros mollis convallis. Donec non facilisis sapien, non ultrices magna. Duis erat neque, congue ac nisl sit amet, viverra imperdiet justo. Mauris id volutpat felis. Curabitur ac imperdiet ligula, vitae molestie enim. Nulla ut tortor venenatis, congue ligula non, iaculis tortor. Phasellus malesuada felis eros, sit amet tristique urna posuere in. Donec tempor magna nisi, et mattis erat blandit et. Phasellus at libero ut arcu dictum placerat vel vitae eros.
+					</p>
+					<p>
+						Mauris odio neque, interdum ut nisl ac, commodo fringilla tortor. Etiam dolor nulla, feugiat eu augue non, tincidunt faucibus sapien. Nullam in risus mauris. Maecenas lacinia, ante sit amet tincidunt porta, erat nisl ultrices eros, quis elementum turpis urna non nulla. Praesent faucibus augue vel massa luctus ullamcorper. Donec et quam sed lacus pulvinar porttitor. Mauris lectus quam, elementum in est cursus, sollicitudin porttitor massa.
+					</p>
+				</section>
+			</div>
+			<div class="medium-4 columns">
+				<section class="sidebar">
+					<h3><i class="fa fa-globe"></i> Statistics</h3>
+					<ul>
+						<li><strong>Total Registered:</strong> 2,000</li>
+						<li><strong>Total Online:</strong> 1,200</li>
+						<li><strong>Total Active:</strong> 1,950</li>
+					</ul>
+					<ul>
+						<li><strong>Richest Player:</strong> TestAccount</li>
+						<li><strong>Strongest Pet:</strong> BestPetEver</li>
+						<li><strong>Best Item:</strong> CoolItem1</li>
+						<li><strong>Amount of Money:</strong> &pound; 5,000,001</li>
+					</ul>
+					<h3><i class="fa fa-bullhorn"></i> Options</h3>
+				</section>
+			</div>
+		</div>
+	</section>
+
+	<section class="blog">
+		<div class="row">
+			<div class="large-12 columns">
+				<h2>Keep watching out!</h2>
+			</div>
+			<div class="medium-4 columns">
+				<div class="box two">
+					<div class="box-header">
+						sdfsdf
+					</div>
+					<div class="box-content">
+						sdfsfsdfsdfsdf
+					</div>
+				</div>
+			</div>
+			<div class="medium-4 columns">
+				<div class="box two">
+					<div class="box-header">
+						sdfsdf
+					</div>
+					<div class="box-content">
+						sdfsfsdfsdfsdf
+					</div>
+				</div>
+			</div>
+			<div class="medium-4 columns">
+				<div class="box two">
+					<div class="box-header">
+						sdfsdf
+					</div>
+					<div class="box-content">
+						sdfsfsdfsdfsdf
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="row">
+			<div class="large-12 columns">
+				dgsd
+			</div>
+		</div>
+	</footer>
+</body>
+
+</html>
